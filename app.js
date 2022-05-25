863b3ac6ffc1323e76aedff6cc5997014e050746
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-05-25 17:19:14 +0200

    Création d'une route GET factice /api/sauces

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-const USERS = require("./routes/users/index.js")
+const USERS = require("./routes/auth/index.js")
 
 const EXPRESS = require("express");
 const APP = EXPRESS();
 
 APP.use((req, res, next) => {
-    console.log("Requête reçue");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
-})
+  });
 
-APP.use((req, res, next) => {
-    res.status(201);
-    next();
-})
-
-APP.use((req, res, next) => {
-    res.json({message: "Votre requête a bien été reçue"});
-    next();
-})
-
-APP.use((req, res) => {
-    console.log("Réponse envoyée avec succès");
-})
+APP.use('/api/sauces', (req, res, next) => {
+    const listOfSauces = [
+        {
+            userId: "abcde",
+            name: "Sauce 1",
+            manufacturer: "abcde",
+            description: "Caliente!",
+            mainPepper: "Cayenne Pepper",
+            imageUrl: "fakeImageUrl1",
+            heat: 6,
+            likes: 1,
+            dislikes: 1,
+            usersLiked: ["abcde"],
+            usersDisliked: ["badguy42"]
+        },
+        {
+            userId: "azerty",
+            name: "Sauce 2",
+            manufacturer: "azerty",
+            description: "Not So(ce) Hot!",
+            mainPepper: "Espelette Pepper",
+            imageUrl: "fakeImageUrl2",
+            heat: 3,
+            likes: 2,
+            dislikes: 1,
+            usersLiked: ["azerty", "abcde"],
+            usersDisliked: ["badguy42"]
+        },
+        {
+            userId: "badguy42",
+            name: "Sauce 3",
+            manufacturer: "badguy42",
+            description: "Badly Hot!",
+            mainPepper: "Jalapeno Pepper",
+            imageUrl: "fakeImageUrl3",
+            heat: 8,
+            likes: 1,
+            dislikes: 0,
+            usersLiked: ["badguy42"],
+            usersDisliked: []
+        },
+    ];
+    res.status(200).json(listOfSauces);
+});
 
 /* APP.use("/api/users", USERS); */
 
