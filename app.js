56f8bde6fb12bab8bb2901d67ff6c21c97aa89fb
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-05-30 11:05:44 +0200

    Implémentation des routes sauces PUT et DELETE

--- app.js
+++ app.js
@@ -46,6 +46,18 @@ app.get('/api/sauces/:id', (req, res, next) => {
         .catch(error => res.status(404).json({error}));
 });
 
+app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: "Sauce modifiée avec succès !"}))
+        .catch(error => res.status(400).json({error}));
+})
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauce.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: "Sauce supprimée avec succès !"}))
+        .catch(error => res.status(400).json({error}));
+})
+
 /* app.use("/api/auth", User); */
 
 module.exports = app;
