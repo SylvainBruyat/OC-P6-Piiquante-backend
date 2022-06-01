35970a964a1e881116b74b83dab9435d4754933a
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-06-01 14:55:56 +0200

    Correction de la route sauces PUT

--- routes/sauces/SauceRouter.js
+++ routes/sauces/SauceRouter.js
@@ -8,7 +8,7 @@ const multer = require('../../middleware/multer-config')
 router.post('/', auth, multer, SauceController.createSauce);
 router.get('/', auth, SauceController.getAllSauces);
 router.get('/:id', auth, SauceController.getOneSauce);
-router.put('/:id', multer, auth, SauceController.modifySauce);
+router.put('/:id', auth, multer, SauceController.modifySauce);
 router.delete('/:id', auth, SauceController.deleteSauce);
 
 module.exports = router;
