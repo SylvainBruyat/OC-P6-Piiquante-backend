e5b40168e9cfe53f059f2ed9a7c3f491105c3009
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-06-08 19:08:07 +0200

    Remplacement des .then().catch() des controllers par des try / catch

--- routes/auth/UserController.js
+++ routes/auth/UserController.js
@@ -3,65 +3,50 @@ const jwt = require('jsonwebtoken');
 
 const User = require('./UserModel').model;
 
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({message: "Utilisateur créé avec succès"}))
-                .catch(error => res.status(400).json({error}));
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({message: "Internal error"});
+exports.signup = async (req, res, next) => {
+    try {
+        let hash = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            email: req.body.email,
+            password: hash
         });
+        user.save()
+            .then(() => res.status(201).json({message: "Compte créé avec succès"}))
+            .catch(error => res.status(400).json({error}));
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal error"});
+    }
 }
 
-exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({error: "Il n'y a pas de compte lié à cette adresse e-mail"});
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(validPassword => {
-                    if (!validPassword) {
-                        return res.status(401).json({error: "Mot de passe incorrect !"});
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            {userId: user._id},
-                            'THi5_IS-My&S3CRET+T0ken*ENcrypTION/key', // Clé temporaire pour le développement. A remplacer en production
-                            {expiresIn: '24h'}
-                        )
-                    });
-                })
-                .catch(error => {
-                    console.error(error);
-                    res.status(500).json({message: "Internal error"});
-                })
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({message: "Internal error"});
-        });
-};
+exports.login = async (req, res, next) => {
+    try {
+        let user = await User.findOne({email: req.body.email})
+        if (!user)
+            return res.status(401).json({error: "Il n'y a pas de compte lié à cette adresse e-mail"});
 
+        bcrypt.compare(req.body.password, user.password)
+            .then(validPassword => {
+                if (!validPassword)
+                    return res.status(401).json({error: "Mot de passe incorrect !"});
 
-
-
-/* module.exports = {
-    signup: async (req, res) => {
-        try {
-            res.send("OK");
-        }
-        catch (error) {
-            console.error(error);
-            res.status(500).send("Internal error");
-        }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        {userId: user._id},
+                        'THi5_IS-My&S3CRET+T0ken*ENcrypTION/key', // Clé temporaire pour le développement. A remplacer en production
+                        {expiresIn: '24h'}
+                    )
+                });
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(500).json({message: "Internal error"});
+            });
     }
-} */
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal error"});
+    }
+}
--- routes/sauces/SauceController.js
+++ routes/sauces/SauceController.js
@@ -2,72 +2,91 @@ const fs = require('fs');
 
 const Sauce = require('./SauceModel').model;
 
-exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-    const sauce = new Sauce({
-        ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        likes: 0,
-        dislikes: 0,
-        usersLiked: [],
-        usersDisliked: []
-    });
-    sauce.save() //Renvoie le produit créé (y compris l'ID) quand la promise est résolue
-        .then(() => res.status(201).json({message: "Sauce créée avec succès !"}))
-        .catch(error => res.status(400).json({error}));
-};
+exports.createSauce = async (req, res, next) => {
+    try {
+        const sauceObject = JSON.parse(req.body.sauce);
+        delete sauceObject._id;
+        const sauce = new Sauce({
+            ...sauceObject,
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+            likes: 0,
+            dislikes: 0,
+            usersLiked: [],
+            usersDisliked: []
+        });
+        await sauce.save(); //Renvoie le produit créé (y compris l'ID) quand la promise est résolue
+        res.status(201).json({message: "Sauce créée avec succès !"});
+    }
+    catch (error) {
+        res.status(400).json({error});
+    }
+}
 
-exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({error}));
-};
+exports.getAllSauces = async (req, res, next) => {
+    try {
+        let sauces = await Sauce.find();
+        res.status(200).json(sauces);
+    }
+    catch (error) {
+        res.status(400).json({error});
+    }
+}
 
-exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error}));
-};
+exports.getOneSauce = async (req, res, next) => {
+    try {
+        let sauce = await Sauce.findOne({_id: req.params.id});
+        res.status(200).json(sauce);
+    }
+    catch (error) {
+        res.status(404).json({error});
+    }
+}
 
-exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?
-        {
-            ...JSON.parse(req.body.sauce),
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        }
-        : {...req.body};
-    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
-        .then(() => res.status(200).json({message: "Sauce modifiée avec succès !"}))
-        .catch(error => res.status(400).json({error}));
-};
+exports.modifySauce = async (req, res, next) => {
+    try {
+        const sauceObject = req.file ?
+            {
+                ...JSON.parse(req.body.sauce),
+                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+            }
+            : {...req.body};
+        await Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id});
+        res.status(200).json({message: "Sauce modifiée avec succès !"});
+    }
+    catch (error) {
+        res.status(400).json({error});
+    }
+}
 
-exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-        .then((sauce) => {
-            if (!sauce)
-                return res.status(404).json({message: "Objet non trouvé !"});
-            if (sauce.userId !== req.auth.userId)
-                return res.status(401).json({message: "Requête non autorisée !"});
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({_id: req.params.id})
-                    .then(() => res.status(200).json({message: "Sauce supprimée avec succès !"}))
-                    .catch(error => res.status(400).json({error}));
-            })
+exports.deleteSauce = async (req, res, next) => {
+    try {
+        let sauce = await Sauce.findOne({_id: req.params.id});
+        
+        if (!sauce)
+            return res.status(404).json({message: "Sauce non trouvée !"});
+
+        if (sauce.userId !== req.auth.userId)
+            return res.status(401).json({message: "Requête non autorisée !"});
+
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({_id: req.params.id})
+                .then(() => res.status(200).json({message: "Sauce supprimée avec succès !"}))
+                .catch(error => res.status(400).json({error}));
         })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({message: "Internal error"});
-        })
-};
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal error"});
+    }
+}
 
 exports.likeDislikeSauce = async (req, res, next) => {
     try {
         let sauce = await Sauce.findOne({_id: req.params.id});
 
         if (!sauce)
-        return res.status(404).json({message: "Objet non trouvé !"});
+            return res.status(404).json({message: "Sauce non trouvée !"});
     
         const userAlreadyLiked = sauce.usersLiked.find(userId => userId == req.auth.userId);
         const userAlreadyDisliked = sauce.usersDisliked.find(userId => userId == req.auth.userId);
@@ -81,23 +100,21 @@ exports.likeDislikeSauce = async (req, res, next) => {
                 }
                 else {
                     await Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: 1}, $push: {usersDisliked: req.auth.userId}});
-                    res.status(201).json({message: "Dislike pris en compte !"});
+                    return res.status(201).json({message: "Dislike pris en compte !"});
                 }
-                break;
             
             case 0:
                 if (userAlreadyDisliked) {
                     await Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: -1}, $pull: {usersDisliked: req.auth.userId}});
-                    res.status(201).json({message: "Dislike supprimé !"});
+                    return res.status(201).json({message: "Dislike supprimé !"});
                 }
                 else if (userAlreadyLiked) {
                     await Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: req.auth.userId}});
-                    res.status(201).json({message: "Like supprimé !"});
+                    return res.status(201).json({message: "Like supprimé !"});
                 }
                 else {
                     return res.status(409).json({message: "Vous n'avez pas de like ou dislike à supprimer pour cette sauce !"});
                 }
-                break;
             
             case 1:
                 if (userAlreadyDisliked) {
@@ -108,9 +125,8 @@ exports.likeDislikeSauce = async (req, res, next) => {
                 }
                 else {
                     await Sauce.updateOne({_id: req.params.id}, {$inc: {likes: 1}, $push: {usersLiked: req.auth.userId}});
-                    res.status(201).json({message: "Like pris en compte !"});
+                    return res.status(201).json({message: "Like pris en compte !"});
                 }
-                break;
         }
     }
     catch (error) {
