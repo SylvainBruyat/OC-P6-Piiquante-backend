e045cf9842fa5163ff54addebb135971bf48818d
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-06-10 15:43:02 +0200

    Mise en place d'une clé secrète pour la génération des JWT

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'THi5_IS-My&S3CRET+T0ken*ENcrypTION/key'); // A remplacer avant de mettre en production (utiliser le .env)
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
         const userId = decodedToken.userId;
         req.auth = {userId};
         if (req.body.userId && req.body.userId !== userId) // Eventuellement à supprimer en déplaçant la gestion du userId dans les controllers
@@ -12,6 +12,6 @@ module.exports = (req, res, next) => {
             next();
     }
     catch (error) {
-        res.status(403).json({error: error || "Requête non authentifiée"});
+        res.status(403).json({error: error || "Requête non-autorisée"});
     }
 };
